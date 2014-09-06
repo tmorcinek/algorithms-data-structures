1b746cd71070c80a20ef13683844521e1ad8dfaf
Tomasz Morcinek <tomasz.morcinek@gmail.com>
2014-09-06 19:52:55 +0200

    Starting implementation of ArrayList.

---
+++ src/main/java/com/morcinek/training/data_structures/list/ArrayList.java
@@ -0,0 +1,32 @@
+package com.morcinek.training.data_structures.list;
+
+/**
+ * Copyright 2014 Tomasz Morcinek. All rights reserved.
+ */
+public class ArrayList<T> implements List<T> {
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public void insert(T object) {
+
+    }
+
+    @Override
+    public T get(int index) {
+        return null;
+    }
+
+    @Override
+    public int search(T object) {
+        return 0;
+    }
+
+    @Override
+    public T delete(int index) {
+        return null;
+    }
+}
