9a41db2525519f124948d476b503ab27baac949e
Tomasz Morcinek <tomasz.morcinek@gmail.com>
2014-09-06 21:26:58 +0200

    Array list implementation, all tests pass.

--- src/main/java/com/morcinek/training/data_structures/list/ArrayList.java
+++ src/main/java/com/morcinek/training/data_structures/list/ArrayList.java
@@ -5,6 +5,10 @@ package com.morcinek.training.data_structures.list;
  */
 public class ArrayList<T> implements List<T> {
 
+    public static final int ARRAY_INITIAL_SIZE = 20;
+
+    private Object[] array = new Object[ARRAY_INITIAL_SIZE];
+
     private int size;
 
     @Override
@@ -14,21 +18,33 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public void insert(T object) {
+        array[size] = object;
         size++;
     }
 
     @Override
     public T get(int index) {
-        return null;
+        return (T) array[index];
     }
 
     @Override
     public int search(T object) {
-        return 0;
+        for (int i = 0; i < size; i++) {
+            if (array[i].equals(object)) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     @Override
     public T delete(int index) {
-        return null;
+        T object = null;
+        if (index < size) {
+            object = (T) array[index];
+            System.arraycopy(array, index + 1, array, index, size - index);
+            size--;
+        }
+        return object;
     }
 }
