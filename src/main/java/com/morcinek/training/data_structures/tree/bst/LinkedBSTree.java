0af0a635cea9a0e91cce1a5bae301491ad695084
Tomasz Morcinek <tomasz.morcinek@gmail.com>
2014-09-08 23:04:50 +0200

    Implementation of InOrderValuesToList method.

--- src/main/java/com/morcinek/training/data_structures/tree/bst/LinkedBSTree.java
+++ src/main/java/com/morcinek/training/data_structures/tree/bst/LinkedBSTree.java
@@ -1,17 +1,66 @@
 package com.morcinek.training.data_structures.tree.bst;
 
+import com.morcinek.training.data_structures.list.ArrayList;
+import com.morcinek.training.data_structures.list.List;
+
 /**
  * Copyright 2014 Tomasz Morcinek. All rights reserved.
  */
 public class LinkedBSTree<T extends Comparable<T>> implements BSTree<T> {
 
+    private class BSTNode {
+        T value;
+        BSTNode left;
+        BSTNode right;
+        BSTNode parent;
+
+        private BSTNode(BSTNode parent, T value) {
+            this.parent = parent;
+            this.value = value;
+        }
+    }
+
+    private BSTNode root;
+
     @Override
     public void insert(T value) {
+        if (root == null) {
+            root = new BSTNode(null, value);
+        } else {
+            BSTNode node = root;
+            while (node != null) {
+                if (value.compareTo(node.value) <= 0) {
+                    if (node.left != null) {
+                        node = node.left;
+                    } else {
+                        node.left = new BSTNode(node, value);
+                        node = null;
+                    }
+                } else {
+                    if (node.right != null) {
+                        node = node.right;
+                    } else {
+                        node.right = new BSTNode(node, value);
+                        node = null;
+                    }
+                }
+            }
 
+        }
     }
 
     @Override
     public Object[] getInOrderValues() {
-        return new Object[0];
+        List<T> values = new ArrayList<T>();
+        getInOrderValuesToList(values, root);
+        return values.toArray();
+    }
+
+    private void getInOrderValuesToList(List<T> list, BSTNode node) {
+        if (node != null) {
+            getInOrderValuesToList(list, node.left);
+            list.insert(node.value);
+            getInOrderValuesToList(list, node.right);
+        }
     }
 }
